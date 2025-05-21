b439f28a2e97a1f9efe8abd9e61c1fb70c4425b5
Juwon-G <wndnnjs123@naver.com>
2025-05-21 17:20:34 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import MainPage from './pages/MainPage'
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import SignupSuccess from './pages/SignupSuccess';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import { AuthProvider, useAuth } from './AuthContext';
 
 function App() {
+  const {isAuthenticated} = useAuth();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/main" /> : <LoginPage />} />
+        <Route path="/main" element={isAuthenticated ? <MainPage /> : <Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signupsuccess" element={<SignupSuccess />} />
+        <Route path="/forgot" element={<ForgotPasswordPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
